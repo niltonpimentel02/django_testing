5cc34a198013e2b5aef4b2081e20b31727ee1042
Nilton Pimentel <niltonpimentel02@gmail.com>
2021-08-01 16:28:44 -0300

    created app store

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'store',
     'django_extensions',
     'debug_toolbar',
 ]
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
 
 if settings.DEBUG:
